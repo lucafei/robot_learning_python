807b19ce96548a3a14b11cc12f4befe5d4f5a1f4
yinfei <yinfei@minieye.cc>
2025-09-15 21:18:19 +0800

    [fix] 跑通lqr算法

--- rl/lqr.py
+++ rl/lqr.py
@@ -23,15 +23,22 @@ class VehicleModel:
 
     def state_space(self, ref_delta, ref_yaw):
         # A = np.array([[1.0, 0.0, -self.v * self.dt *])
-        pass
-
+        A = np.matrix([
+            [1.0,0.0,-self.v*self.dt*math.sin(ref_yaw)],
+            [0.0,1.0,self.v*self.dt*math.cos(ref_yaw)],
+            [0.0,0.0,1.0]])
+        B = np.matrix([
+            [self.dt*math.cos(ref_yaw),0],
+            [self.dt*math.sin(ref_yaw),0],
+            [self.dt*math.tan(ref_delta)/self.L, self.v*self.dt/(self.L*math.cos(ref_delta)*math.cos(ref_delta))]])
+        return A, B
 class LQR:
     def __init__(self):
         pass
 
     def calRicatti(self, A, B, Q, R):
         max_iter = 500
-        eps = 0.01
+        eps = 1e-4
         Qf = Q
         P = Qf
         for iter in range(max_iter):
@@ -76,49 +83,67 @@ def GenerateReferenceLine():
     y = a*x**5 + b*x**4 + c*x**3 + d*x**2 + e*x + f
 
     # 计算theta角
-
+    y_dot = 5*a*x**4 + 4*b*x**3 + 3*c*x**2 + 2*d*x + e
+    theta = np.arctan(y_dot)
 
     # 计算曲率k
-
+    k = (20*a*x**3 + 12*b*x**2 + 6*c*x + 2*d) / ((1 + y_dot**2)**1.5)
     
     # 返回参考路径点
-    ref_line = np.column_stack((x, y))
+    ref_line = np.column_stack((x, y, theta, k))
     return ref_line
 
 def calcNearestPoint(x, y, reference_line):
     min_d = float('inf')
     nearest_point = [x, y]
-    for [rx, ry] in  reference_line:
+    for [rx, ry, rtheta, rk] in  reference_line:
         dx = x - rx
         dy = y - ry
         d = np.hypot(dx, dy)
         if d < min_d:
             min_d = d
-            nearest_point = [rx, ry]
-    return nearest_point, min_d
+            nearest_point = [rx, ry, rtheta, rk]
+    return nearest_point
 
 if __name__ == "__main__":
-    ugv = VehicleModel(0, 0, 0, 0, 1, 0.1)
+    ugv = VehicleModel(0, -1, 0, 2, 1, 0.1)
     lqr = LQR()
     ref_line = GenerateReferenceLine()
-    figure = plt.figure()
-    ax = figure.add_subplot(111)
-    ax.plot(ref_line[:, 0], ref_line[:, 1], color='blue')
+    figure = plt.figure(1)
+    # ax = figure.add_subplot(111)
+    # ax.plot(ref_line[:, 0], ref_line[:, 1], color='blue')
 
     init_state = [1, 0]
+    Q = np.diag([3, 3, 3])
+    R = np.diag([2, 2])
+    x_ = []
+    y_ = []
     for i in range(500):
         robot_state = np.zeros(4)
         robot_state[0] = ugv.x
         robot_state[1] = ugv.y
         robot_state[2] = ugv.psi
         robot_state[3] = ugv.v
-        e, k, ref_yaw= calcNearestPoint(robot_state[0], robot_state[1], ref_line)
-        ref_delta = math.atan2(L*k, 1)
+        x, y, ref_yaw, k = calcNearestPoint(robot_state[0], robot_state[1], ref_line)
+        ref_delta = math.atan2(ugv.L * k, 1)
         A, B = ugv.state_space(ref_delta, ref_yaw)
-        delta = lqr.lqrcontrol(robot_state, )
+        delta = lqr.lqrcontrol(robot_state, [x, y,ref_yaw], A, B, Q, R)
         delta += ref_delta
+        delta = np.clip(delta, -np.pi/4, np.pi/4)
         ugv.update_state(0, delta)
+        x_.append(ugv.x)
+        y_.append(ugv.y)
 
-    plt.show()
+
+        plt.cla()
+        plt.plot(ref_line[:, 0], ref_line[:, 1], "-.b", linewidth=1.0, label="course")
+        plt.plot(x_,y_,"-r",label="trajectory")
+        plt.grid(True)
+        plt.pause(0.001)
+
+        if np.linalg.norm(robot_state[0:2] - ref_line[-1, 0:2])<=0.1:
+            print("reach goal")
+            break
+    # plt.show()
 
 
